14230f28bab3e010fcbb84e42d6d260e099c8bb3
Lucius Choi <lucius.choi@gmail.com>
2023-08-19 04:23:51 +0900

    added balloon action to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -57,6 +57,8 @@ class PostsController < ApplicationController
     end
   end
 
+  def balloon; end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
