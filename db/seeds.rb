f8ca426f4ecd375b5d8775b4f56fa6073d0b130c
Lucius Choi <lucius.choi@gmail.com>
2023-08-19 04:10:56 +0900

    Added posts using seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.create title: 'Sollicitudin Nullam Fermentum', content: 'Donec sed odio dui. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+Post.create title: 'Sit Adipiscing Nullam', content: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+Post.create title: 'Venenatis Tristique Mattis', content: 'Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper.'
+Post.create title: 'Ligula Porta Risus', content: 'Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.'
+Post.create title: 'Dolor Justo Cras', content: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+
