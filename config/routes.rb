18a687e5be5f0fec6870f12b8cb1ab23d79196ff
Lucius Choi <lucius.choi@gmail.com>
2023-08-19 04:43:38 +0900

    fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "posts#index"
   resources :posts
-  get "ballon", to: "posts#balloon", as: :ballon
+  get "ballon", to: "posts#balloon", as: :balloon
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
